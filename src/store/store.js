b7c2d342d845544e7d86174461762a214ea7090a
Kevin Kipp <kevin.kipp@gmail.com>
2017-07-01 12:46:53 -0500

    Apply redux-localstorage middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import * as redux from 'redux';
 import { reducer } from '../reducers/reducers';
 import { initialState } from './initial-state';
+import persistState from 'redux-localstorage'
 
 export function configureStore(state = initialState) {
   return redux.createStore(reducer, state, redux.compose(
+    persistState(),
     window.devToolsExtension ? window.devToolsExtension() : f => f
   ));
 }
