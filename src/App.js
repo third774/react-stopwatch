911a6e17f376ddb89f66f38886b02a5e02917b66
Kevin Kipp <kevin.kipp@gmail.com>
2017-07-01 15:40:07 -0500

    Remove logo import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import logo from './logo.svg';
 import StopwatchControls from './components/StopwatchControls';
 import Timer from './components/Timer';
 
