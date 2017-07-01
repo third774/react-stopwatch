4318b819c9bf6f6c5d8a143bbdfab1dafbd0ad50
Kevin Kipp <kevin.kipp@gmail.com>
2017-07-01 12:07:22 -0500

    Move formatTime function into helpers.js

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../styles/Timer.scss';
+import { formatTime } from '../helpers/helpers';
 
 export class Timer extends Component {
   render() {
@@ -11,19 +12,3 @@ export class Timer extends Component {
 }
 
 export default connect(state => state)(Timer);
-
-function formatTime(time) {
-  let minutes = Math.floor(time / 60000);
-  let seconds = Math.floor((time % 60000) / 1000);
-  let milliSeconds = Math.floor((time % 1000) * 0.1);
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
-  if (milliSeconds < 10) {
-    milliSeconds = `0${milliSeconds}`;
-  }
-  return `${minutes}:${seconds}.${milliSeconds}`;
-}
