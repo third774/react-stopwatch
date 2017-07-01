8698b4b0895a58f02fbe54d1367824c05a7a017c
Kevin Kipp <kevin.kipp@gmail.com>
2017-07-01 12:09:48 -0500

    Wire up Timer to update with requestAnimationFrame

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './styles/App.scss';
 import StopwatchControls from './components/StopwatchControls';
 import Timer from './components/Timer';
 
 class App extends Component {
+
+  get totalTime() {
+    if (this.props.started) {
+      return Date.now() - this.props.started + this.props.recordedTime;
+    } else {
+      return this.props.recordedTime;
+    }
+  }
+  
+  componentDidUpdate() {
+    if (this.props.started) {
+      this.animationFrame = requestAnimationFrame(this.tick.bind(this));
+    } else {
+      cancelAnimationFrame(this.animationFrame);
+    }
+  }
+
+  tick() {
+    this.forceUpdate();
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className="app-title">React Stopwatch</h1>
-        <Timer time={168196}/>
+        <Timer time={this.totalTime}/>
         <StopwatchControls />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(store => store)(App);
