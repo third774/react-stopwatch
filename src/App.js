597cf333ce40e7c87b85986290fec0634fa5f038
Kevin Kipp <kevin.kipp@gmail.com>
2017-07-01 12:10:27 -0500

    Update app title class using BEM

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="app-title">React Stopwatch</h1>
+        <h1 className="app__title">React Stopwatch</h1>
         <Timer time={this.totalTime}/>
         <StopwatchControls />
       </div>
