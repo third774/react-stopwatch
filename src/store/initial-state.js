81e5e3a3a5ef72569476f6fba4491daf4443f4da
Kevin Kipp <kevin.kipp@gmail.com>
2017-07-02 15:38:38 -0500

    Update comments in initialState
    Change milliseconds to centiseconds

--- src/store/initial-state.js
+++ src/store/initial-state.js
@@ -1,24 +1,24 @@
 export const initialState = {
   
   /**
-   * Epoch timestamp of when the timer was started
+   * Epoch timestamp of when the timer was started in centiseconds
    */
   started: null,
 
   /**
-   * Total time recorded when stopping the timer in milliseconds
+   * Total time recorded when stopping the timer in centiseconds
    */
   recordedTime: 0,
 
   /**
-   * Sum of lap times in milliseconds
+   * Sum of lap times in centiseconds
    * Setting this value on each lap so it doesn't need to be
    * calculated when rendering
    */
   lapTotal: 0,
 
   /**
-   * Array of lap times in milliseconds. New laps are prepended, so the list is in reverse
+   * Array of lap times in centiseconds. New laps are prepended, so the list is in reverse
    */
   laps: []
 };
