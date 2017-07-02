f095b6bce5f68d0b77e0f63ec0218fcc231b2ee5
Kevin Kipp <kevin.kipp@gmail.com>
2017-07-01 19:17:45 -0500

    Add font awesome icons

--- src/components/StopwatchControls.js
+++ src/components/StopwatchControls.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { startTimer, stopTimer, addLap, resetTimer } from '../actions/actions';
+import FontAwesome from 'react-fontawesome';
 
 export class StopwatchControls extends Component {
   render() {
@@ -9,15 +10,23 @@ export class StopwatchControls extends Component {
     if (started) {
       return (
         <div className="stopwatch-controls">
-          <button className="button button--red" onClick={() => dispatch(stopTimer(Date.now()))}>Stop</button>
-          <button className="button button--blue" onClick={() => dispatch(addLap(Date.now()))}>Lap</button>
+          <button className="button button--red" onClick={() => dispatch(stopTimer(Date.now()))}>
+            <FontAwesome className="fa-fw" name='square'/> Stop
+          </button>
+          <button className="button button--blue" onClick={() => dispatch(addLap(Date.now()))}>
+            <FontAwesome className="fa-fw" name='refresh'/> Lap
+          </button>
         </div>
       )
     } else {
       return (
         <div className="stopwatch-controls">
-          <button className="button button--green" onClick={() => dispatch(startTimer(Date.now()))}>Start</button>
-          <button className="button button--red" onClick={() => dispatch(resetTimer())}>Reset</button>
+          <button className="button button--green" onClick={() => dispatch(startTimer(Date.now()))}>
+            <FontAwesome className="fa-fw" name='play'/> Start
+          </button>
+          <button className="button button--red" onClick={() => dispatch(resetTimer())}>
+            <FontAwesome className="fa-fw" name='trash'/> Reset
+          </button>
         </div>
       )
     }
